af61bc5be0b5199c93d40a189a4dbacb80f50995
山南 <zhangpeichaook@163.com>
2016-06-06 19:42:06 +0800

    DataPager not completed

--- DoingWell/UserControls/DataPager.xaml.cs
+++ DoingWell/UserControls/DataPager.xaml.cs
@@ -15,14 +15,83 @@ using System.Windows.Shapes;
 
 namespace Shannan.DoingWell.UserControls
 {
-    /// <summary>
-    /// DataPager.xaml 的交互逻辑
-    /// </summary>
     public partial class DataPager : UserControl
     {
+        #region Constants
+        private const string ElementRoot = "PART_Root";
+        private const string ElementFirstButton = "PART_FirstButton";
+        private const string ElementLastButton = "PART_LastButton";
+        private const string ElementPreviousButton = "PART_PreviousButton";
+        private const string ElementNextButton = "PART_NextButton";
+        #endregion Constants
+
+        #region Data
+        private Button _firstButton;
+        private Button _lastButton;
+        private Button _previousButton;
+        private Button _nextButton;
+        #endregion Data
+
         public DataPager()
         {
             InitializeComponent();
+
+            this.Loaded += DataPager_Loaded;
+
         }
+
+        #region Private Properties
+        internal Button FirstButton
+        {
+            get { return this._firstButton; }
+        }
+        internal Button LastButton
+        {
+            get { return this._lastButton; }
+        }
+        internal Button PreviousButton
+        {
+            get { return this._previousButton; }
+        }
+        internal Button NextButton
+        {
+            get { return this._nextButton; }
+        }
+        #endregion Private Properties
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if (this._firstButton != null)
+            {
+                this._firstButton.Click -= new RoutedEventHandler(FirstButton_Click);
+            }
+
+            this._firstButton = GetTemplateChild(ElementFirstButton) as Button;
+
+            if (this._firstButton != null)
+            {
+                if (this._firstButton.Content == null)
+                {
+                    this._firstButton.Content = "首页";
+                }
+                this._firstButton.Click += new RoutedEventHandler(FirstButton_Click);
+            }
+
+
+
+        }
+
+        private void FirstButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void DataPager_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
     }
 }
