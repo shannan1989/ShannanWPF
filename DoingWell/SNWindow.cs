c922f3d945927777d40e33bb1a228c555bce423e
山南 <zhangpeichaook@163.com>
2016-07-15 11:23:47 +0800

    Default Window

---
+++ DoingWell/SNWindow.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Shannan.DoingWell
+{
+    public class SNWindow : Window
+    {
+        public SNWindow()
+        {
+            Loaded += SNWindow_Loaded;
+            PreviewMouseDoubleClick += SNWindow_PreviewMouseDoubleClick;
+
+            ShowInTaskbar = true;
+            WindowState = WindowState.Maximized;
+            //ResizeMode = ResizeMode.NoResize;
+        }
+
+        private void SNWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Width = SystemParameters.WorkArea.Width;
+            Height = SystemParameters.WorkArea.Height;
+        }
+
+        private void SNWindow_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
