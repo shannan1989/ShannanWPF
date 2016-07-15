b13f35f1e484a1efa1265e7f0c79a509a257b40f
山南 <zhangpeichaook@163.com>
2016-07-15 11:22:24 +0800

    ControlsWindow
    Display user controls and styles for controls

---
+++ DoingWell/ControlsWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace Shannan.DoingWell
+{
+    public partial class ControlsWindow : SNWindow
+    {
+        public ControlsWindow()
+        {
+            InitializeComponent();
+
+            Loaded += ControlsWindow_Loaded;
+        }
+
+        private void ControlsWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            pager.CurrentPageChanged += Pager_CurrentPageChanged;
+        }
+
+        private void Pager_CurrentPageChanged(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show(pager.CurrentPage.ToString());
+        }
+
+    }
+}
