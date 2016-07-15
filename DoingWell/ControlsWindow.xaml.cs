9ac867a5710d82c0e4b5caa42e30f1d7a7cf66fa
山南 <zhangpeichaook@163.com>
2016-07-15 11:42:00 +0800

    Controls Window

--- DoingWell/ControlsWindow.xaml.cs
+++ DoingWell/ControlsWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace Shannan.DoingWell
 {
@@ -14,6 +16,8 @@ namespace Shannan.DoingWell
         private void ControlsWindow_Loaded(object sender, RoutedEventArgs e)
         {
             pager.CurrentPageChanged += Pager_CurrentPageChanged;
+
+            imageViewer.Source = new BitmapImage(new Uri("http://uploads.xuexila.com/allimg/1507/641-150G31I335.jpg"));
         }
 
         private void Pager_CurrentPageChanged(object sender, RoutedEventArgs e)
