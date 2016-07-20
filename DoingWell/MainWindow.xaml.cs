ef6aafa83f3a6f68b017662911eb60df7e5229e8
山南 <zhangpeichaook@163.com>
2016-07-20 10:43:22 +0800

    MainWindow

--- DoingWell/MainWindow.xaml.cs
+++ DoingWell/MainWindow.xaml.cs
@@ -1,21 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.NetworkInformation;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Shannan.DoingWell
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : SNWindow
     {
         public MainWindow()
         {
@@ -26,17 +16,86 @@ namespace Shannan.DoingWell
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            pager.CurrentPageChanged += Pager_CurrentPageChanged;
+            string ov = "操作系统版本：（" + Environment.OSVersion + "）" + Environment.NewLine;
+            ov += "Platform：" + Environment.OSVersion.Platform + Environment.NewLine;
+            ov += "Version：" + Environment.OSVersion.Version + Environment.NewLine;
+            ov += "VersionString：" + Environment.OSVersion.VersionString + Environment.NewLine;
+            ov += "ServicePack：" + Environment.OSVersion.ServicePack + Environment.NewLine;
+            if (Environment.Is64BitOperatingSystem)
+                ov += "64 Bit Operating System" + Environment.NewLine;
+            else
+                ov += "32 Bit Operating System" + Environment.NewLine;
+            OSVersionInfo.Text = ov;
+
+            string version = ".Net Framework版本号：（" + Environment.Version + "）" + Environment.NewLine;
+            version += "Major：" + Environment.Version.Major + Environment.NewLine;
+            version += "MajorRevision：" + Environment.Version.MajorRevision + Environment.NewLine;
+            version += "Minor：" + Environment.Version.Minor + Environment.NewLine;
+            version += "MinorRevision：" + Environment.Version.MinorRevision + Environment.NewLine;
+            version += "Build：" + Environment.Version.Build + Environment.NewLine;
+            version += "Revision：" + Environment.Version.Revision + Environment.NewLine;
+            VersionInfo.Text = version;
+
+            ResolutionInfo.Text = "Resolution：" + string.Format("{0}*{1}", SystemParameters.PrimaryScreenWidth, SystemParameters.PrimaryScreenHeight) + Environment.NewLine;
+
+            WorkAreaInfo.Text = "WorkArea：" + string.Format("{0}*{1}", SystemParameters.WorkArea.Width, SystemParameters.WorkArea.Height) + Environment.NewLine;
+
+            SystemDirectoryInfo.Text = "SystemDirectory：" + Environment.SystemDirectory + Environment.NewLine;
+
+            CurrentDirectoryInfo.Text = "应用程序当前目录：" + Environment.CurrentDirectory + Environment.NewLine;
+
+            string drivesInfo = "Drives：" + Environment.NewLine;
+            string[] drives = Environment.GetLogicalDrives();
+            foreach (string drive in drives)
+            {
+                drivesInfo += drive + Environment.NewLine;
+            }
+            DrivesInfo.Text = drivesInfo;
+
+            string networks = "Networks：" + Environment.NewLine;
+            string currentNetwork = string.Empty;
+            foreach (NetworkInterface adapter in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                networks += adapter.Name + "----" + adapter.Id + "----" + adapter.Description + "----" + adapter.Speed + Environment.NewLine;
+                if (currentNetwork == string.Empty && adapter.OperationalStatus == OperationalStatus.Up)
+                {
+                    currentNetwork = adapter.Name;
+                }
+            }
+            NetworksInfo.Text = networks;
+            CurrentNetworksInfo.Text = "CurrentNetwork：" + currentNetwork + Environment.NewLine;
+
+            MachineNameInfo.Text = "MachineName：" + Environment.MachineName;
+            UserNameInfo.Text = "UserName：" + Environment.UserName;
+            UserDomainNameInfo.Text = "UserDomainName：" + Environment.UserDomainName;
+            ProcessorCountInfo.Text = "ProcessorCount：" + Environment.ProcessorCount;
+            TickCountInfo.Text = "TickCount：" + Environment.TickCount;
+
+            DesktopInfo.Text = "Desktop：" + Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            //Environment.GetFolderPath(Environment.SpecialFolder对象);
+            //Environment.SpecialFolder对象提供系统保留文件夹标识，例如: Environment.SpecialFolder.Desktop表示桌面文件夹的路径。
         }
 
-        private void Pager_CurrentPageChanged(object sender, RoutedEventArgs e)
+        private void NavigationButton_Click(object sender, RoutedEventArgs e)
         {
-            //MessageBox.Show(pager.CurrentPage.ToString());
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            pager.Total = 200;
+            string tag = (sender as Button).Tag.ToString();
+            switch (tag)
+            {
+                case "VideoPlayer":
+                    Window videoWindow = new VideoWindow();
+                    videoWindow.ShowDialog();
+                    break;
+                case "DrawingBoard":
+                    Window drawingWindow = new DrawingWindow();
+                    drawingWindow.ShowDialog();
+                    break;
+                case "Controls":
+                    Window controlsWindow = new ControlsWindow();
+                    controlsWindow.ShowDialog();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
