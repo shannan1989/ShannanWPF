9988b864b093f4fabcadb30fa69554e455ade5e4
山南 <zhangpeichaook@163.com>
2016-07-28 15:13:16 +0800

    ProgressRing

---
+++ DoingWell/UserControls/ProgressRing.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Shannan.DoingWell.UserControls
+{
+    public partial class ProgressRing : UserControl
+    {
+        public ProgressRing()
+        {
+            InitializeComponent();
+        }
+    }
+}
