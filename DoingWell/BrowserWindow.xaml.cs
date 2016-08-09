d84943c5432ac22d7527f76e71b877121838529a
山南 <zhangpeichaook@163.com>
2016-08-09 09:57:17 +0800

    BrowserWindow

--- DoingWell/BrowserWindow.xaml.cs
+++ DoingWell/BrowserWindow.xaml.cs
@@ -21,16 +21,17 @@ namespace Shannan.DoingWell
             webControl.TitleChanged += WebControl_TitleChanged;
             webControl.AddressChanged += WebControl_AddressChanged;
             webControl.ConsoleMessage += WebControl_ConsoleMessage;
+            webControl.ShowContextMenu += WebControl_ShowContextMenu;
+            webControl.NativeViewInitialized += WebControl_NativeViewInitialized;
+            webControl.WindowClose += WebControl_WindowClose;
         }
 
         private void BrowserWindow_ContentRendered(object sender, EventArgs e)
         {
-
         }
 
         private void WebControl_DocumentReady(object sender, DocumentReadyEventArgs e)
         {
-
         }
 
         private void WebControl_ShowCreatedWebView(object sender, ShowCreatedWebViewEventArgs e)
@@ -40,17 +41,27 @@ namespace Shannan.DoingWell
 
         private void WebControl_ConsoleMessage(object sender, ConsoleMessageEventArgs e)
         {
-
         }
 
         private void WebControl_AddressChanged(object sender, UrlEventArgs e)
         {
-
         }
 
         private void WebControl_TitleChanged(object sender, TitleChangedEventArgs e)
         {
             Title = e.Title;
+        }
+
+        private void WebControl_WindowClose(object sender, WindowCloseEventArgs e)
+        {
+        }
+
+        private void WebControl_NativeViewInitialized(object sender, WebViewEventArgs e)
+        {
+        }
+
+        private void WebControl_ShowContextMenu(object sender, ContextMenuEventArgs e)
+        {
         }
     }
 }
