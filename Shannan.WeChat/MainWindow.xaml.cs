fd8da8ec4bbfe3ccea9be89a92778fefb34c1310
山南 <zhangpeichaook@163.com>
2016-11-17 19:13:14 +0800

    MainWindow

---
+++ Shannan.WeChat/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace Shannan.WeChat
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += delegate
+            {
+            };
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
+            base.OnClosing(e);
+        }
+    }
+}
