58d411bc8e33059d298322ff2fddd41f2bff6e55
山南 <zhangpeichaook@163.com>
2016-11-17 19:12:16 +0800

    Init

---
+++ Shannan.WeChat/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace Shannan.WeChat
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Current.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+            LoginWindow lw = new LoginWindow();
+            bool? lwResult = lw.ShowDialog();
+            if (lwResult.HasValue && lwResult.Value == true)
+            {
+                MainWindow window = new MainWindow();
+                window.Show();
+            }
+            else
+            {
+                Shutdown();
+            }
+        }
+    }
+}
