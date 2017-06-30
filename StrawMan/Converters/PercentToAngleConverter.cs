eb51c31ab104e334c04fe2f0f8abc7a9ae5059df
山南 <zhangpeichaook@163.com>
2017-06-30 15:23:51 +0800

    PercentToAngleConverter

---
+++ StrawMan/Converters/PercentToAngleConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Shannan.StrawMan.Converters
+{
+    internal class PercentToAngleConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return 360 * (double)values[0] / (double)values[1];
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
