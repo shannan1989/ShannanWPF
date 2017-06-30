970296b9e6c0d92b17eece8c3f820327784838a0
山南 <zhangpeichaook@163.com>
2017-06-30 15:30:55 +0800

    Dialog for displaying styles

---
+++ StrawMan/Scenes/Controls/DStyles.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Shannan.StrawMan
+{
+    public partial class DStyles : SNDialog
+    {
+        public DStyles()
+        {
+            InitializeComponent();
+
+            Width = SystemParameters.WorkArea.Width * 0.9;
+            Height = SystemParameters.WorkArea.Height * 0.9;
+            WindowStartupLocation = WindowStartupLocation.CenterOwner;
+        }
+    }
+}
