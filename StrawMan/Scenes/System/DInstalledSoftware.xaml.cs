2f38d5502161cf9b01c5988f52c36d367b6a7b3d
山南 <zhangpeichaook@163.com>
2017-06-30 09:39:25 +0800

    Dialog for installed softwares

---
+++ StrawMan/Scenes/System/DInstalledSoftware.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Text;
+using System.Threading;
+using System.Windows;
+
+namespace Shannan.StrawMan
+{
+    public partial class DInstalledSoftware : SNDialog
+    {
+        public DInstalledSoftware()
+        {
+            InitializeComponent();
+
+            WindowStartupLocation = WindowStartupLocation.CenterOwner;
+
+            Loaded += delegate
+            {
+                //GetInstalledSoftwares();
+                Application.Current.Dispatcher.BeginInvoke(new Action(delegate
+                {
+                    GetInstalledSoftwares();
+                }));
+            };
+        }
+
+        private void GetInstalledSoftwares()
+        {
+            StringBuilder info = new StringBuilder();
+            string keyStr = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall";
+
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(keyStr))
+            {
+                foreach (string subKeyName in key.GetSubKeyNames())
+                {
+                    info.AppendLine(subKeyName);
+                    using (RegistryKey subKey = key.OpenSubKey(subKeyName))
+                    {
+                        foreach (string name in subKey.GetValueNames())
+                        {
+                            info.AppendLine("---- " + name + " : " + subKey.GetValue(name));
+                        }
+                    }
+                    info.AppendLine();
+                }
+            }
+
+            info.AppendLine("----------------------------------------------------------");
+
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(keyStr))
+            {
+                foreach (string subKeyName in key.GetSubKeyNames())
+                {
+                    info.AppendLine(subKeyName);
+                    using (RegistryKey subKey = key.OpenSubKey(subKeyName))
+                    {
+                        foreach (string name in subKey.GetValueNames())
+                        {
+                            info.AppendLine("---- " + name + " : " + subKey.GetValue(name));
+                        }
+                    }
+                    info.AppendLine();
+                }
+            }
+
+            Thread.Sleep(1000);
+            ApplicationsInfo.Text = info.ToString();
+        }
+    }
+}
