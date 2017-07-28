ec0f94a0213d119018efdde23263a11a7b998c57
山南 <zhangpeichaook@163.com>
2017-07-28 14:49:08 +0800

    ObjectConverter

---
+++ StrawMan/Converters/ObjectConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace Shannan.StrawMan.Converters
+{
+    internal sealed class ObjectConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string[] param = parameter.ToString().ToLower().Split(':');
+            if (value == null)
+            {
+                return param[2];
+            }
+            if (param[0].Contains("|"))
+            {
+                return param[0].Split('|').Contains(value.ToString().ToLower()) ? param[1] : param[2];
+            }
+            return param[0].Equals(value.ToString().ToLower()) ? param[1] : param[2];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
